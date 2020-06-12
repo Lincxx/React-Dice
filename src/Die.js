4ebe82ae8c6af2f4b76d108ec327881e9b07fa4f
Lincxx <bossco28@gmail.com>
2020-06-12 14:29:10 -0400

    First Commit and First Component

---
+++ src/Die.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './Die.css'
+
+class Die extends Component {
+  render() {
+    let cls = `Die fas fa-dice-${this.props.face}`
+    return (
+           
+        <i className={cls}></i>
+    
+    )
+  }
+}
+
+export default Die
