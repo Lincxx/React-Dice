a0ddd372dc835ad3e2810812db5de4c4d22f15ed
Lincxx <bossco28@gmail.com>
2020-06-12 15:32:14 -0400

    Added in some layout style

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Die from './Die'
+import './Rolldice.css'
 
 class RollDice extends Component {
   static defaultProps = {
@@ -26,9 +27,12 @@ class RollDice extends Component {
 
   render() {
     return (
-      <div>
-        <Die face={this.state.die1}/>
-        <Die face={this.state.die2} />
+      <div className="Rolldice">
+        <div className="Rolldice-container">
+          <Die face={this.state.die1} />
+          <Die face={this.state.die2} />
+        </div>
+        
         <button onClick={this.roll}>Roll Dice</button>
       </div>
     )
